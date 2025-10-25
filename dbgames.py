9e9f060fb3f340461268b3c9e6b5761d392dcab5
[Alan Bantug] <[alan.n.bantug@gmail.com]>
2025-10-25 08:06:50 -0700

    change plays qualifier

--- dbgames.py
+++ dbgames.py
@@ -255,7 +255,7 @@ class Application(Frame):
         if self.selPlayed.get():
 
             count = int(self.varCount.get())
-            tact_text = f'plays >= {count} '
+            tact_text = f'plays = {count} '
             if where_count == 0:
                 select_sql = self.add_where(0, select_sql, tact_text)
             else:
